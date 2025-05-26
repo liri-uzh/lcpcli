08d21e99762cc8eacdbe84a31b4f051361c710bf
Jeremy Zehr <jeremy.zehr@uzh.ch>
2025-05-26 16:57:02 +0200

    Bump to v0.2.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="lcpcli",
-    version="0.2.1",
+    version="0.2.2",
     description="CLI tool combining LCP Upload and LCP Corpert",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
